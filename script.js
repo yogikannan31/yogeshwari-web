cb9c5d9989233166f4bda6bdae1a4c0db514dc12
yogeshwari l <yyogi9064@gmail.com>
2026-01-21 15:57:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.getElementById("adPopup").style.display = "flex";
+};
+
+function closeAd() {
+    document.getElementById("adPopup").style.display = "none";
+}
